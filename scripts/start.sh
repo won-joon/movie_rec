493e3b46d498dbc2c8cc22a8a59e8936186bb41c
WonJoon <seowj@gmail.com>
2024-03-13 23:05:41 +0900

    Upgrade gradle.yml

--- scripts/start.sh
+++ scripts/start.sh
@@ -12,8 +12,10 @@ IDLE_PORT=$(find_idle_port)
 
 echo "$TIME_NOW > Build 파일 복사" >> $DEPLOY_LOG
 
-
 cp $REPOSITORY/build/libs/*.jar $JAR_FILE      # 새로운 jar file 계속 덮어쓰기
+
+echo "$TIME_NOW > $JAR_FILE 에 실행권한 추가" >> $DEPLOY_LOG
+chmod +x $JAR_NAME
 
 echo "$TIME_NOW > JAR Name: $JAR_FILE" >> $DEPLOY_LOG
 echo "$TIME_NOW > $JAR_FILE 실행" >> $DEPLOY_LOG
